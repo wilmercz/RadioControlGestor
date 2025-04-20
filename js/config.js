ccdb95c6fc3eb50434fcb1cd9da674da5632bb4f
wilmercz <49212228+wilmercz@users.noreply.github.com>
2025-04-20 18:05:45 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,18 @@
-// config.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD0lkEDKhia76E6ef3uU0B1lOH8CyklCnc",
+  apiKey: "AIzaSyAdWnEkNfuJcQNSIIMHGfLb_HEcGQ9Ctw0",
   authDomain: "arki-service-autoridades.firebaseapp.com",
   databaseURL: "https://arki-service-autoridades.firebaseio.com",
   projectId: "arki-service-autoridades",
   storageBucket: "arki-service-autoridades.appspot.com",
   messagingSenderId: "829954736380",
-  appId: "1:829954736380:web:c4badfee0e0db02dd69779"
+  appId: "1:829954736380:web:97228b09bae0ea04d69779"
 };
 
-// Inicializar Firebase
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
