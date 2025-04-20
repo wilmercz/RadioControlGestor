7145d497abf69c1f6a89f5e16b7a6e5284b1600b
wilmercz <49212228+wilmercz@users.noreply.github.com>
2025-04-20 17:05:12 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "TU_API_KEY",
+    authDomain: "tu-proyecto.firebaseapp.com",
+    databaseURL: "https://tu-proyecto.firebaseio.com",
+    projectId: "tu-proyecto",
+    storageBucket: "tu-proyecto.appspot.com",
+    messagingSenderId: "TU_MESSAGING_ID",
+    appId: "TU_APP_ID"
+};
+
+// Inicializar Firebase
+firebase.initializeApp(firebaseConfig);
